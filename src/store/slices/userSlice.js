5879f48ec906390e4905f75f1b372e42e61bd879
raval555 <aurasmusicstudio@gmail.com>
2025-02-22 14:03:33 +0530

    Create userSlice.js

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  userInfo: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    clearUserInfo: (state) => {
+      state.userInfo = null;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+    clearError: (state) => {
+      state.error = null;
+    },
+  },
+});
+
+export const {
+  setUserInfo,
+  clearUserInfo,
+  setLoading,
+  setError,
+  clearError,
+} = userSlice.actions;
+
+export default userSlice.reducer;
