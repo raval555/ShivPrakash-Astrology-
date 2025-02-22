bf8c47b31dbca8352de453f89121db0b94cdd78b
raval555 <aurasmusicstudio@gmail.com>
2025-02-22 14:43:15 +0530

    Create Apsp.js

---
+++ src/Apsp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import AppRoutes from './routes';
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <AppRoutes />
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
